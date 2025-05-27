2551b8c6513d1fc691e2770cec37de1a713e9c27
Gil Aguilar <redeyedev.208@gmail.com>
2025-05-26 21:24:08 -0600

    Initializing project with package dependencies

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul></ul>
+</template>
+
+<script>
+export default {
+  data() {
+    storedResources: [
+      {
+        id: 'vue-3-official-guide',
+        title: 'Official Guide',
+        description: 'The official Vue.js (Vue 3) documentation',
+        link: 'https://vuejs.org',
+      },
+      {
+        id: 'google',
+        title: 'Google',
+        description: 'Learn to google...',
+        link: 'https://google.org',
+      },
+    ];
+  },
+};
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
